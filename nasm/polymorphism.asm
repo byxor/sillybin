f52bb4cf0d90924104f00de279f397a20f0a68b9
byxor <brandnewbyxor@gmail.com>
2018-06-16 18:43:12 +0100

    Add polymorphism example

---
+++ nasm/polymorphism.asm
@@ -0,0 +1,77 @@
+extern printf
+
+
+section .text
+        global main
+
+
+main:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, callback0
+        call invokePolymorphically
+
+        mov rdi, callback1
+        call invokePolymorphically
+
+        mov rax, 0
+        pop rbp
+        ret
+
+
+;; rdi = callback
+section .rodata
+        invokePolymorphically_startMessage    db "Invoking callback...", 10, 0
+        invokePolymorphically_endMessage      db "Done!", 10, 10, 0
+section .text
+invokePolymorphically:
+        push rbp
+        mov rbp, rsp
+
+        sub rsp, 8
+        mov [rbp - 8], rdi
+
+        mov rdi, invokePolymorphically_startMessage
+        mov al, 0
+        call printf
+
+        call [rbp - 8]
+
+        mov rdi, invokePolymorphically_endMessage
+        mov al, 0
+        call printf
+
+        add rsp, 8
+        pop rbp
+        ret
+
+
+section .rodata
+        callback0_message       db "Callback 0", 10, 0
+section .text
+callback0:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, callback0_message
+        mov al, 0
+        call printf
+
+        pop rbp
+        ret
+
+
+section .rodata
+        callback1_message       db "Now that's what I call Callback 1", 10, 0
+section .text
+callback1:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, callback1_message
+        mov al, 0
+        call printf
+
+        pop rbp
+        ret
