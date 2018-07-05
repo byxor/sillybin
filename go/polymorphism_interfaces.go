1bdc452748af10107ff78137d678f0ccde996bc9
byxor <brandnewbyxor@gmail.com>
2018-07-05 05:58:32 +0100

    Add interface polymorphism example

--- go/polymorphism_interfaces.go
+++ go/polymorphism_interfaces.go
@@ -4,3 +4,40 @@ import (
 	"fmt"
 	"strings"
 )
+
+func main() {
+	adapters := []adapter{angryAdapter{3}, angryAdapter{5}, vapeAdapter{}}
+	for _, a := range adapters {
+		adapt(a, "Hello")
+	}
+}
+
+// --- Abstract ---
+
+type adapter interface {
+	Adapt(string) string
+}
+
+func adapt(a adapter, s string) {
+	fmt.Println(a.Adapt(s))
+}
+
+// --- Concrete ---
+
+type angryAdapter struct {
+	exclamations int
+}
+
+func (a angryAdapter) Adapt(s string) string {
+	return strings.ToUpper(s) + strings.Repeat("!", a.exclamations)
+}
+
+type vapeAdapter struct{}
+
+func (a vapeAdapter) Adapt(s string) string {
+	output := ""
+	for _, r := range []rune(s) {
+		output += string(r) + " "
+	}
+	return output
+}
