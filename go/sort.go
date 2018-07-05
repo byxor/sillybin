0006c73975267a395d2a83efd849934a4a53560c
byxor <brandnewbyxor@gmail.com>
2018-07-05 04:06:11 +0100

    Add bubble sort

---
+++ go/sort.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	numbers := getNumbers()
+
+	sort(numbers)
+
+	fmt.Println(numbers)
+}
+
+func getNumbers() []int64 {
+	fmt.Print("Enter numbers: ")
+	line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	line = line[:len(line)-1]
+
+	segments := strings.Split(line, " ")
+
+	numbers := make([]int64, len(segments))
+	for i, segment := range segments {
+		numbers[i], _ = strconv.ParseInt(segment, 10, 32)
+	}
+
+	return numbers
+}
+
+func sort(numbers []int64) {
+	numOfSorted := 0
+	length := len(numbers)
+
+	for numOfSorted < length {
+		i := 0
+		for i < length-numOfSorted-1 {
+			a := numbers[i]
+			b := numbers[i+1]
+			if a > b {
+				numbers[i] = b
+				numbers[i+1] = a
+			}
+			i++
+		}
+		numOfSorted++
+	}
+}
