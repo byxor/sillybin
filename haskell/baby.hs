66502efc268aaea0cf02908896537f68a5a918ea
byxor <brandnewbyxor@gmail.com>
2018-12-09 18:12:36 +0000

    Add haskell

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -5,3 +5,13 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+numbers = [1,2,3,4,5]
+
+one = head numbers
+
+five = last numbers
