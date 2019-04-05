73397c289bed1e2e1f137aa1d273b8bf2b2845a8
byxor <brandnewbyxor@gmail.com>
2019-04-05 20:36:29 +0100

    Begin writing protobuf example with grpc

---
+++ protobuf/compile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script requires GRPC code generators (for java and javascript).
+#   Java: https://search.maven.org/search?q=g:io.grpc%20a:protoc-gen-grpc-java
+#   JavaScript:
+
+# These code generators need to be placed into your "pluginDirectory":
+javaGrpcPlugin="$HOME/software/bin/protoc-gen-grpc-java-1.19.0-linux-x86_64.exe"
+
+######################################################################
+
+protoFile="Library.proto"
+javaOutput="java"
+jsOutput="js"
+
+######################################################################
+
+echo "Compiling to Java..."
+
+rm -rf "$javaOutput"
+mkdir -p "$javaOutput"
+
+protoc \
+  --plugin=protoc-gen-grpc-java="$javaGrpcPlugin" \
+  --grpc-java_out="$javaOutput" \
+  --java_out="$javaOutput" \
+  "$protoFile"
+
+######################################################################
+
+echo "Compiling to JavaScript..."
+
+rm -rf "$jsOutput"
+mkdir -p "$jsOutput"
+
+protoc \
+  --js_out="$jsOutput" \
+  "$protoFile"
+
+######################################################################
+
+echo "done."
