0ceff53387d4141889e2a3d024eeaf3cbe1da2bb
byxor <brandnewbyxor@gmail.com>
2019-04-06 17:12:07 +0100

    Add javascript grpc client

---
+++ protobuf/client.js
@@ -0,0 +1,39 @@
+var PROTO_PATH = __dirname + '/Library.proto';
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+);
+
+var library_proto = grpc.loadPackageDefinition(packageDefinition) // .helloworld;
+
+function main() {
+  var client = new library_proto.SearchService(
+      'localhost:50051',
+      grpc.credentials.createInsecure()
+  );
+
+  var user = 'Bob';
+
+  client.search(
+      {text: 'the bible'},
+      function(err, response) {
+          if (!!err) {
+              console.log('Err:', err);
+          } else {
+              console.log('Book:', response.title, response.author);
+          }
+      }
+  );
+}
+
+main();
